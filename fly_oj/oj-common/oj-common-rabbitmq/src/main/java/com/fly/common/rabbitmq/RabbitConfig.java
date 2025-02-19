ebf5476cdaefe5d2609eca2f1a8706b0113091bc
7flyx <2647282717@qq.com>
2025-02-19 22:37:02 +0800

    mq配置

--- fly_oj/oj-common/oj-common-rabbitmq/src/main/java/com/fly/common/rabbitmq/RabbitConfig.java
+++ fly_oj/oj-common/oj-common-rabbitmq/src/main/java/com/fly/common/rabbitmq/RabbitConfig.java
@@ -1,2 +1,22 @@
-package com.fly.common.rabbitmq;public class RabbitConfig {
+package com.fly.common.rabbitmq;
+
+import com.fly.common.core.constants.RabbitMQConstants;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitConfig {
+    @Bean
+    public Queue workQueue() {
+        return new Queue(RabbitMQConstants.OJ_WORK_QUEUE, true);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
