a9f9e750677adcf39db493c240133fd724341b0f
7flyx <2647282717@qq.com>
2025-02-19 22:41:26 +0800

    公共类、常量等

--- fly_oj/oj-common/oj-common-core/src/main/java/com/fly/common/core/constants/RabbitMQConstants.java
+++ fly_oj/oj-common/oj-common-core/src/main/java/com/fly/common/core/constants/RabbitMQConstants.java
@@ -1,2 +1,5 @@
-package com.fly.common.core.constants;public class RabbitMQConstants {
+package com.fly.common.core.constants;
+
+public class RabbitMQConstants {
+    public static final String OJ_WORK_QUEUE = "oj-work-queue";
 }
--- fly_oj/oj-common/oj-common-core/src/main/java/com/fly/common/core/enums/QuestionResType.java
+++ fly_oj/oj-common/oj-common-core/src/main/java/com/fly/common/core/enums/QuestionResType.java
@@ -1,2 +1,19 @@
-package com.fly.common.core.enums;public enum QuestionResType {
+package com.fly.common.core.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum QuestionResType {
+    ERROR(0), // 错误
+    PASS(1), // 通过
+    UN_SUBMIT(2), // 未提交
+    IN_JUDGE(3); // 正在判题
+
+
+
+    private Integer value;
+
+    QuestionResType(Integer value) {
+        this.value = value;
+    }
 }
